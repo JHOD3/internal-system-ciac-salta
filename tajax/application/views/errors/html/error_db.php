eb9dc36ed05efde59552ef1be380e928e56a0207
Diego Garcia <diegog368@gmail.com>
2017-07-12 11:21:00 -0300

    agregar www en direcciones ciac

--- tajax/application/views/errors/html/error_db.php
+++ tajax/application/views/errors/html/error_db.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta http-equiv="content-type" content="text/html" />
     <meta charset="UTF-8" />
 	<title>TURNOS ONLINE CIAC | Centro Integral de Alta Complejidad</title>
-    <link rel="shortcut icon" type="image/x-icon" href="http://ciacsalta.com.ar/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="http://www.ciacsalta.com.ar/favicon.ico">
     <link rel="stylesheet" href="assets/css/ciac.css" />
     <link rel="stylesheet" href="assets/css/media.queries.ciac.css" />
     <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
