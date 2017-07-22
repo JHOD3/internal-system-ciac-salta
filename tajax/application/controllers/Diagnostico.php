b833439cd3bc8dbe7e94746e5c30260cf2c3809a
Diego García <diego368@hotmail.com>
2017-07-22 11:16:07 -0300

    permitir peticiones ajax de dyndns

--- tajax/application/controllers/Diagnostico.php
+++ tajax/application/controllers/Diagnostico.php
@@ -6,6 +6,7 @@ class Diagnostico extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
+        $this->output->set_header('Access-Control-Allow-Origin: *');
         $this->load->model($this->router->fetch_class().'_model', 'Model');
     }
 
