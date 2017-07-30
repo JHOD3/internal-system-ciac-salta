af5f5db458556bff2a059af3fcedcd212bdbe19b
Diego Garcia <diegog368@gmail.com>
2017-07-30 18:46:48 -0300

    hacer url relativa y no absoluta

--- tajax/application/views/diagnostico/Listado_view.php
+++ tajax/application/views/diagnostico/Listado_view.php
@@ -75,7 +75,7 @@
     </tfoot>
 </table>
 <div>
-<a href="../index.php/<?=$this->router->fetch_class().'/exportar/'.$date1.'/'.$date2?>">Exportar Listado</a>
+<a href="../tajax/index.php/<?=$this->router->fetch_class().'/exportar/'.$date1.'/'.$date2?>">Exportar Listado</a>
 </div>
 
 <div id="tab_medicos" class="tab_hidden">
