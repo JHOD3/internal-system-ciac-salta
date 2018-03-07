934b7f3ab229fc917b7d5225e3e07c1e048db276
Diego Garcia <diegog368@gmail.com>
2018-03-07 12:40:49 -0300

    mejorar el turnero web

--- tajax/application/views/calendar_view.php
+++ tajax/application/views/calendar_view.php
@@ -1,148 +1,156 @@
-<div><?=$calendar?></div>
-<div class="detail">
-    <div class="title">
-        <?php if (is_array($aHorarios)): ?>
-            Horarios disponibles para el <span><?=$day?>/<?=$month?>/<?=$year?></span>
-            <input type="hidden" id="fecha" name="fecha" value="<?=$year?>-<?=$month?>-<?=$day?>" />
-        <?php else: ?>
-            <br />No hay Horarios disponibles para el <span><?=$day?>/<?=$month?>/<?=$year?></span>
-        <?php endif; ?>
-        <?php if (count($aHorariosInhabilitados) > 0): ?>
-            <br />
-            <span style="font-weight: normal;">Los horarios Ocupados se encuentran reservados y no pueden ser otorgados</span>
-            <br />
-            <?php $cnct = 'Motivo: '; ?>
-            <?php foreach ($aHorariosInhabilitados AS $rsHI): ?>
-                <?php if ($rsHI['motivo_descripcion'] != 'Otro'): ?>
-                    <?=$cnct?><?=$rsHI['motivo_descripcion']?>
-                    <?php $cnct = '<span style="font-weight: normal;">,</span> '; ?>
-                <?php else: ?>
-                    <?=$cnct?>El Médico no puede asistir
-                    <?php $cnct = '<span style="font-weight: normal;">,</span> '; ?>
-                <?php endif; ?>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
-    <?php if (is_array($aHorarios) and isset($aHorarios[0])): ?>
-        <div class="atencion">
-            <?php if (in_array($aHorarios[0]['id_turnos_tipos'], array(3, 4))): ?>
-                Estimado paciente, la atención con <?=doSaludo($rsMedico)?> se realiza por orden de llegada.
-                Por favor diríjase a mesa de entrada
-            <?php elseif (in_array($aHorarios[0]['id_turnos_tipos'], array(5, 6))): ?>
-                Estimado paciente para reserva de turno con <?=doSaludo($rsMedico)?>,
-                por favor diríjase a mesa de entrada de lunes a viernes de 07:30 a 21:00 hs
-            <?php elseif (in_array($aHorarios[0]['id_turnos_tipos'], array(7, 8))): ?>
-                Estimado paciente los turnos con <?=doSaludo($rsMedico)?> son asignados exclusivamente por el Profesional,
-                por favor diríjase a mesa de entrada de lunes a viernes de 07:30 a 21:00 hs
+<div class="sPicon fa fa-calendar"><strong>Elija un Turno</strong></div>
+<div class="clearfloat"></div>
+<div class="padding-05em-15px col-md-12" style="background-color: #f3f3f3;">
+    <div><?=$calendar?></div>
+    <div class="detail">
+        <div class="MedEsp">
+            <span class="fa fa-user-md"></span>
+            <?=doSaludo($rsMedico, false)?>&nbsp;&nbsp;&nbsp;
+            <span class="fa fa-stethoscope"></span>
+            <?=$rsEspecialidad['nombre']?>
+        </div>
+        <div class="title">
+            <?php if (is_array($aHorarios)): ?>
+                Horarios disponibles para el <span><?=$day?>/<?=$month?>/<?=$year?></span>
+                <input type="hidden" id="fecha" name="fecha" value="<?=$year?>-<?=$month?>-<?=$day?>" />
+            <?php else: ?>
+                <br />No hay Horarios disponibles para el <span><?=$day?>/<?=$month?>/<?=$year?></span>
             <?php endif; ?>
-            <?php if (in_array($aHorarios[0]['id_turnos_tipos'], array(3, 4, 5, 6, 7, 8))): ?>
-                <div>
-                    <span>
-                        Horario de atención
-                        <?=
-                        (substr(doSaludo($rsMedico), 0, 2) == 'el' ? 'd' : 'de ').
-                        doSaludo($rsMedico)
-                        ?>:<br />
-                        <?=$vcDiasHorarios?>
-                    </span>
-                </div>
+            <?php if (count($aHorariosInhabilitados) > 0): ?>
+                <br />
+                <span style="font-weight: normal;">Los horarios Ocupados se encuentran reservados y no pueden ser otorgados</span>
+                <br />
+                <?php $cnct = 'Motivo: '; ?>
+                <?php foreach ($aHorariosInhabilitados AS $rsHI): ?>
+                    <?php if ($rsHI['motivo_descripcion'] != 'Otro'): ?>
+                        <?=$cnct?><?=$rsHI['motivo_descripcion']?>
+                        <?php $cnct = '<span style="font-weight: normal;">,</span> '; ?>
+                    <?php else: ?>
+                        <?=$cnct?>El Médico no puede asistir
+                        <?php $cnct = '<span style="font-weight: normal;">,</span> '; ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             <?php endif; ?>
         </div>
-        <?php if (in_array($aHorarios[0]['id_turnos_tipos'], array(1, 2))): ?>
-            <div class="leyendadisp">
-                <div class="ocup">Ocupado&nbsp;</div>
-                <div class="disp">Disponible&nbsp;</div>
+        <?php if (is_array($aHorarios) and isset($aHorarios[0])): ?>
+            <div class="atencion">
+                <?php if (in_array($aHorarios[0]['id_turnos_tipos'], array(3, 4))): ?>
+                    Estimado paciente, la atención con <?=doSaludo($rsMedico)?> se realiza por orden de llegada.
+                    Por favor diríjase a mesa de entrada
+                <?php elseif (in_array($aHorarios[0]['id_turnos_tipos'], array(5, 6))): ?>
+                    Estimado paciente para reserva de turno con <?=doSaludo($rsMedico)?>,
+                    por favor diríjase a mesa de entrada de lunes a viernes de 07:30 a 21:00 hs
+                <?php elseif (in_array($aHorarios[0]['id_turnos_tipos'], array(7, 8))): ?>
+                    Estimado paciente los turnos con <?=doSaludo($rsMedico)?> son asignados exclusivamente por el Profesional,
+                    por favor diríjase a mesa de entrada de lunes a viernes de 07:30 a 21:00 hs
+                <?php endif; ?>
+                <?php if (in_array($aHorarios[0]['id_turnos_tipos'], array(3, 4, 5, 6, 7, 8))): ?>
+                    <div>
+                        <span>
+                            Horario de atención
+                            <?=
+                            (substr(doSaludo($rsMedico), 0, 2) == 'el' ? 'd' : 'de ').
+                            doSaludo($rsMedico)
+                            ?>:<br />
+                            <?=$vcDiasHorarios?>
+                        </span>
+                    </div>
+                <?php endif; ?>
             </div>
-            <div class="hours">
-                <?php foreach ($aHorarios AS $rsH): ?>
-                    <?php
-                    if (in_array($rsH['id_turnos_tipos'], array(3, 4))) {
-                        $Tx = "Turno 1";
-                    } else {
-                        $Tx = "";
-                    }
-                    ?>
-                    <?php for ($h = $rsH['desde']; $h <= $rsH['hasta']; $h = horaMM($h, $vcDuracionTurno)): ?>
-                        <?php if (in_array($h, $aTurnosReservados)): ?>
-                            <div data-alt="Ocupado"><?=$Tx ? $Tx : substr($h, 0, 5)?></div>
-                        <?php else: ?>
-                            <?php
-                            $boHI = false;
-                            for ($hi = 0; $hi < count($aHorariosInhabilitados); $hi++) {
-                                if (
-                                    isset($aHorariosInhabilitados[$hi]['desde']) and
-                                    $aHorariosInhabilitados[$hi]['desde'] and
-                                    isset($aHorariosInhabilitados[$hi]['hasta']) and
-                                    $aHorariosInhabilitados[$hi]['hasta'] and
-                                    $h >= $aHorariosInhabilitados[$hi]['desde'] and
-                                    $h < $aHorariosInhabilitados[$hi]['hasta']
-                                ) {
-                                    $boHI = true;
-                                }
-                            }
-                            if ($boHI):
-                            ?>
-                                <div data-alt="Ocupado"><?=$Tx ? $Tx : substr($h, 0, 5)?></div>
-                            <?php else: ?>
-                                <?php if (date("Ymd") == $year.$month.$day and $h <= date("H:i:s", strtotime("+3 hours"))): ?>
-                                    <div data-alt="Ocupado"><?=$Tx ? $Tx : substr($h, 0, 5)?></div>
-                                <?php else: ?>
-                                    <a href=""><div data-alt="Disponible"><?=$Tx ? $Tx : substr($h, 0, 5)?></div></a>
-                                <?php endif; ?>
-                            <?php endif; ?>
-                        <?php endif; ?>
+            <?php if (in_array($aHorarios[0]['id_turnos_tipos'], array(1, 2))): ?>
+                <div class="leyendadisp">
+                    <div class="ocup">Ocupado&nbsp;</div>
+                    <div class="disp">Disponible&nbsp;</div>
+                </div>
+                <div class="hours">
+                    <?php foreach ($aHorarios AS $rsH): ?>
                         <?php
                         if (in_array($rsH['id_turnos_tipos'], array(3, 4))) {
-                            $Tx++;
+                            $Tx = "Turno 1";
+                        } else {
+                            $Tx = "";
                         }
                         ?>
-                    <?php endfor; ?>
-                    <br /><br />
-                <?php endforeach; ?>
-            </div>
-            <input type="hidden" id="desde" name="desde" value="" />
-        <?php endif; ?>
-    <?php endif; ?>
-</div>
-<div class="clearfloat"></div>
-<div>
-    <?php
-    $aOSrec_ids = array();
-    if (count($rsObrasSocialesDeMedico) > 0):
-        foreach ($rsObrasSocialesDeMedico AS $rsOSM):
-            $aOSrec_ids[] = $rsOSM['id_obras_sociales'];
-        endforeach;
-    endif;
-    ?>
-    <?php /*
-    <div class="osTitle">Obras Sociales que recibe este Profesional:</div>
-    <div class="osList">
-        <?php $aOSrec_ids = array(); ?>
-        <?php if (count($rsObrasSocialesDeMedico) > 0): ?>
-            <?php foreach ($rsObrasSocialesDeMedico AS $rsOSM): ?>
-                <?php $aOSrec_ids[] = $rsOSM['id_obras_sociales']; ?>
-                <span title="<?=$rsOSM['nombre']?>"><?=$rsOSM['abreviacion']?>,&nbsp;</span>
-            <?php endforeach; ?>
-        <?php else: ?>
-            Atención sin obra social
+                        <?php for ($h = $rsH['desde']; $h <= $rsH['hasta']; $h = horaMM($h, $vcDuracionTurno)): ?>
+                            <?php if (in_array($h, $aTurnosReservados)): ?>
+                                <div data-alt="Ocupado"><?=$Tx ? $Tx : substr($h, 0, 5)?></div>
+                            <?php else: ?>
+                                <?php
+                                $boHI = false;
+                                for ($hi = 0; $hi < count($aHorariosInhabilitados); $hi++) {
+                                    if (
+                                        isset($aHorariosInhabilitados[$hi]['desde']) and
+                                        $aHorariosInhabilitados[$hi]['desde'] and
+                                        isset($aHorariosInhabilitados[$hi]['hasta']) and
+                                        $aHorariosInhabilitados[$hi]['hasta'] and
+                                        $h >= $aHorariosInhabilitados[$hi]['desde'] and
+                                        $h < $aHorariosInhabilitados[$hi]['hasta']
+                                    ) {
+                                        $boHI = true;
+                                    }
+                                }
+                                if ($boHI):
+                                ?>
+                                    <div data-alt="Ocupado"><?=$Tx ? $Tx : substr($h, 0, 5)?></div>
+                                <?php else: ?>
+                                    <?php if (date("Ymd") == $year.$month.$day and $h <= date("H:i:s", strtotime("+3 hours"))): ?>
+                                        <div data-alt="Ocupado"><?=$Tx ? $Tx : substr($h, 0, 5)?></div>
+                                    <?php else: ?>
+                                        <a href=""><div data-alt="Disponible"><?=$Tx ? $Tx : substr($h, 0, 5)?></div></a>
+                                    <?php endif; ?>
+                                <?php endif; ?>
+                            <?php endif; ?>
+                            <?php
+                            if (in_array($rsH['id_turnos_tipos'], array(3, 4))) {
+                                $Tx++;
+                            }
+                            ?>
+                        <?php endfor; ?>
+                        <br /><br />
+                    <?php endforeach; ?>
+                </div>
+                <input type="hidden" id="desde" name="desde" value="" />
+            <?php endif; ?>
         <?php endif; ?>
     </div>
-    */ ?>
-    <?php if (isset($aObrasSociales) and count($aObrasSociales) > 0): ?>
-    <div id="osrec">
-        <div class="osTitle">Elige la Obra Social</div>
-        <span class="custom-dropdown custom-dropdown--white">
-            <select class="custom-dropdown__select custom-dropdown__select--white" id="id_obras_sociales" name="id_obras_sociales">
+    <div class="clearfloat"></div>
+    <div>
+        <?php
+        $aOSrec_ids = array();
+        if (count($rsObrasSocialesDeMedico) > 0):
+            foreach ($rsObrasSocialesDeMedico AS $rsOSM):
+                $aOSrec_ids[] = $rsOSM['id_obras_sociales'];
+            endforeach;
+        endif;
+        ?>
+        <?php /*
+        <div class="osTitle">Obras Sociales que recibe este Profesional:</div>
+        <div class="osList">
+            <?php $aOSrec_ids = array(); ?>
+            <?php if (count($rsObrasSocialesDeMedico) > 0): ?>
+                <?php foreach ($rsObrasSocialesDeMedico AS $rsOSM): ?>
+                    <?php $aOSrec_ids[] = $rsOSM['id_obras_sociales']; ?>
+                    <span title="<?=$rsOSM['nombre']?>"><?=$rsOSM['abreviacion']?>,&nbsp;</span>
+                <?php endforeach; ?>
+            <?php else: ?>
+                Atención sin obra social
+            <?php endif; ?>
+        </div>
+        */ ?>
+        <?php if (isset($aObrasSociales) and count($aObrasSociales) > 0): ?>
+        <div id="osrec">
+            <div class="osTitle">Elige la Obra Social</div>
+            <select class="form-control" id="id_obras_sociales" name="id_obras_sociales">
                 <option value="">-- Seleccione una Obra Social --</option>
                 <?php foreach ($aObrasSociales AS $rsOS): ?>
                     <option id="opt<?=$rsOS['id_obras_sociales']?>" class="rec<?=in_array($rsOS['id_obras_sociales'], $aOSrec_ids)?>" value="<?=$rsOS['id_obras_sociales']?>"><?=$rsOS['abreviacion']?> - <?=ucwords(lower($rsOS['nombre']))?></option>
                 <?php endforeach; ?>
             </select>
-        </span>
-        <div id="id_obras_sociales_div"></div>
+            <div id="id_obras_sociales_div"></div>
+        </div>
+        <?php endif; ?>
+        <div class="clearfloat10"></div>
     </div>
-    <?php endif; ?>
-    <div class="clearfloat10"></div>
 </div>
 
 <script>
---
+++ tajax/application/views/turnero_search_view.php
@@ -0,0 +1,74 @@
+<?php if (count($aMedicos) > 0): ?>
+    <div class="row margin-10">
+        <?php foreach ($aMedicos AS $rsM): ?>
+            <div class="col-sm-6 col-md-4">
+                <div class="txtBig">
+                    <span class="fa fa-user-md" aria-hidden="true"></span>
+                    <strong><?=doSaludo($rsM, false)?></strong>
+                </div>
+                <?php foreach ($rsM['especialidades'] AS $rwEsp): ?>
+                    <div>
+                        <a
+                            href="#"
+                            class="onClickButtonAgenda"
+                            data-id_medicos="<?=$rsM['id_medicos']?>"
+                            data-id_especialidades="<?=$rwEsp['id_especialidades']?>"
+                        >
+                            <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+                            <?=upper($rwEsp['nombre'])?>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
+                <div class="clearfloat30"></div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <script>
+    $(function(){
+        $('.onClickButtonAgenda').click(function(event){
+            event.preventDefault();
+            id_especialidades = $(this).data('id_especialidades');
+            id_medicos = $(this).data('id_medicos');
+            $('#id_medicos').val(id_medicos);
+            $('#id_especialidades').val(id_especialidades);
+            $('#divLoading').html('<div class="opacityBackground"><div class="loading"><img src="assets/images/loading.gif" alt="" /></div></div>');
+            if (typeof ajxM != "undefined" && ajxM && ajxM.readyState != 4){
+                ajxM.abort();
+            }
+            ajxM = $.ajax({
+                url:
+<?php if ($_SERVER['HTTP_HOST'] == 'localhost' or $_SERVER['HTTP_HOST'] == '192.168.0.10'): ?>
+                    'http://<?=$_SERVER['HTTP_HOST']?>/dgadmin/dgadmin2016/ciac/sistema/tajax/index.php/turnero/calendar/' +
+<?php else: ?>
+                    'http://ciacsaltadb.ddns.net/tajax/index.php/turnero/calendar/' +
+<?php endif; ?>
+                    id_especialidades +
+                    '/' +
+                    id_medicos +
+                    '/<?=isset($post['fecha']) ? date("Y/m/d", strtotime($post['fecha'])) : date("Y/m/d")?>'
+                    <?php if (isset($post['desde'])): ?>
+                        +'/<?=$post['desde']?>'
+                    <?php endif; ?>
+                ,
+                context: document.body
+            }).done(function(data) {
+                $('#calendar').html(data);
+                $('#divLoading').html('');
+            }).fail(function(jqXHR, textStatus, errorThrown ){
+                if (textStatus == 'abort'){
+                    $('#calendar').html('<div style="width:100%;text-align:center;">Espere unos segundos por favor...</div>');
+                } else {
+                    $('#calendar').html('<div style="width:100%;text-align:center;">Hubo un problema de conexión. Por favor reintente mas tarde.</div>');
+                    $('#divLoading').html('');
+                }
+            });
+        });
+    });
+    </script>
+<?php else: ?>
+    <div class="row margin-10">
+        <div class="col-md-6">
+            No se encontraron resultados con los filtros seleccionados
+        </div>
+    </div>
+<?php endif; ?>
